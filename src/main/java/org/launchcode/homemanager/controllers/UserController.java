6f7ee41c59da7a29a0160f62c220a4eecf505416
schwifty <jcmcmanemin@gmail.com>
2017-11-29 11:36:12 -0600

    created proccessing handler for registration with validation for username uniqueness and matching passwords. still need to refactor handler to compare and store hashed passwords for security, as well as regex to test for valid email

--- src/main/java/org/launchcode/homemanager/controllers/UserController.java
+++ src/main/java/org/launchcode/homemanager/controllers/UserController.java
@@ -1,5 +1,6 @@
 package org.launchcode.homemanager.controllers;
 
+import org.launchcode.homemanager.models.User;
 import org.launchcode.homemanager.models.data.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -40,7 +41,54 @@ public class UserController {
     @RequestMapping(value = "register", method = RequestMethod.GET)
     public String displayRegister(Model model) {
         model.addAttribute("title", "Register");
+        model.addAttribute("usernameError", "Cats!");
 
         return "user/register";
+    }
+
+    @RequestMapping(value = "register", method = RequestMethod.POST)
+    public String processRegister(@RequestParam String username,
+                                  @RequestParam String password,
+                                  @RequestParam String confirmPassword,
+                                  @RequestParam String email,
+                                  Model model) {
+
+        User newUser = new User();
+
+        boolean usernameTaken = false;
+        boolean passwordsMatch = false;
+
+        String usernameError = "";
+        String passwordError = "";
+
+        for (User user : userDao.findAll()) {
+            if (username == user.getName()) {
+                usernameTaken = true;
+                usernameError = "Username already taken!";
+            }
+
+        }
+        //TODO: change to hash
+        if (password == confirmPassword) {
+            passwordsMatch = true;
+        } else {
+            passwordError = "Passwords do not match";
+        }
+
+        if (!usernameTaken && passwordsMatch) {
+            newUser.setName(username);
+            newUser.setPasswordHash(password); //TODO: change to hash
+            newUser.setEmail(email);
+
+            userDao.save(newUser);
+
+            return "redirect:";
+        } else {
+            model.addAttribute("usernameError", usernameError);
+            model.addAttribute("passwordError", passwordError);
+
+            return "user/register";
+        }
+
     }
 }

f07b47e3759c037427991e73d2fae5531c9f6bec
schwifty <jcmcmanemin@gmail.com>
2018-01-03 19:54:22 -0600

    added spring mail dependencies

--- src/main/java/org/launchcode/homemanager/controllers/UserController.java
+++ src/main/java/org/launchcode/homemanager/controllers/UserController.java
@@ -5,14 +5,13 @@ import org.launchcode.homemanager.models.data.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.mindrot.jbcrypt.BCrypt;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+
 
 @Controller
 @RequestMapping(value = "user")

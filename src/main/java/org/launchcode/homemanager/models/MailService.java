62ef59aad5588b57d5aa6c4b3d0c94769fd8bbdd
schwifty <jcmcmanemin@gmail.com>
2018-01-22 13:59:26 -0600

    added sender methods to MailService class

--- src/main/java/org/launchcode/homemanager/models/MailService.java
+++ src/main/java/org/launchcode/homemanager/models/MailService.java
@@ -2,20 +2,49 @@ package org.launchcode.homemanager.models;
 
 import org.launchcode.homemanager.models.data.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+@Service
 public class MailService {
 
     @Autowired
     private UserDao userDao;
 
-    private SimpleMailMessage email;
+    @Autowired
+    private MailSender mailSender;
 
-    private String fromEmail = "home.manager.test.1@gmail.com";
 
-    private String toEmail;
+    private String[] findEmails () {
+        ArrayList<String> users = new ArrayList();
+        for (User user : userDao.findAll()) {
+            users.add(user.getEmail());
+        }
+        String[] userEmails = users.toArray(new String[users.size()]);
 
-    private Iterable users = userDao.findAll();
+        return userEmails;
+    }
+
+    public void send(String subject, String text) {
+        SimpleMailMessage email = new SimpleMailMessage();
+        String[] toEmails = findEmails();
+        email.setTo(toEmails);
+        email.setSubject(subject);
+        email.setText(text);
+
+        mailSender.send(email);
+    }
+
+    public void send(User user, String subject, String text) {
+        SimpleMailMessage email = new SimpleMailMessage();
+        String to = user.getEmail();
+        email.setTo(to);
+        email.setSubject(subject);
+        email.setText(text);
+
+        mailSender.send(email);
+    }
 }

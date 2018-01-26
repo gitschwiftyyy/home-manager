1ae616965dd4cae03e5c4f840b9db094d93d283a
schwifty <jcmcmanemin@gmail.com>
2018-01-25 18:08:34 -0600

    added notification service to internet

--- src/main/java/org/launchcode/homemanager/controllers/InternetController.java
+++ src/main/java/org/launchcode/homemanager/controllers/InternetController.java
@@ -1,7 +1,10 @@
 package org.launchcode.homemanager.controllers;
 
 import org.launchcode.homemanager.models.BudgetMonth;
+import org.launchcode.homemanager.models.MailService;
+import org.launchcode.homemanager.models.User;
 import org.launchcode.homemanager.models.data.BudgetMonthDao;
+import org.launchcode.homemanager.models.data.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +19,12 @@ public class InternetController {
 
     @Autowired
     BudgetMonthDao budgetMonthDao;
+
+    @Autowired
+    UserDao userDao;
+
+    @Autowired
+    MailService mailService;
 
     @RequestMapping(value = "/budget/{year}/{month}/internet", method = RequestMethod.GET)
     public String displayInternet(Model model,
@@ -38,11 +47,20 @@ public class InternetController {
     @RequestMapping(value = "/budget/{year}/{month}/internet", method = RequestMethod.POST, params = {"update"})
     public String processInternet(@PathVariable String year,
                                   @PathVariable String month,
-                                  @RequestParam String updateAmountString) {
+                                  @RequestParam String updateAmountString,
+                                  @CookieValue(value = "loggedInCookie", required = false) String loggedInUserId) {
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        int userId = Integer.parseInt(loggedInUserId);
+        User thisUser = userDao.findOne(userId);
+
         BudgetMonth thisBudgetMonth = budgetMonthDao.findByYearAndMonth(Integer.parseInt(year), BudgetMonth.monthInt(month));
         Double updateAmount = Double.parseDouble(updateAmountString);
         thisBudgetMonth.setInternet(updateAmount);
         budgetMonthDao.save(thisBudgetMonth);
+
+        String emailSubject = "Budget updated for " + month + " " + year;
+        String emailText = thisUser.getName() + " updated " + month + "'s internet amount to " + formatter.format(updateAmount);
+        mailService.sendToAll(emailSubject, emailText);
         return "redirect:/budget";
     }
 
